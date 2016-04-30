4211902ac284134e8851afc16c789a2ef6f2f930
Thomas Elliott <tom.w.elliott@gmail.com>
2016-04-30 19:31:52 -0400

    Add a quick icon from fontawesome

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -4,6 +4,24 @@ package routes
 import "github.com/revel/revel"
 
 
+type tApi struct {}
+var Api tApi
+
+
+func (_ tApi) Unsubscribe(
+		owner string,
+		repo string,
+		dryRun bool,
+		) string {
+	args := make(map[string]string)
+	
+	revel.Unbind(args, "owner", owner)
+	revel.Unbind(args, "repo", repo)
+	revel.Unbind(args, "dryRun", dryRun)
+	return revel.MainRouter.Reverse("Api.Unsubscribe", args).Url
+}
+
+
 type tApplication struct {}
 var Application tApplication
 
@@ -34,24 +52,6 @@ func (_ tAuth) Auth(
 	
 	revel.Unbind(args, "code", code)
 	return revel.MainRouter.Reverse("Auth.Auth", args).Url
-}
-
-
-type tApi struct {}
-var Api tApi
-
-
-func (_ tApi) Unsubscribe(
-		owner string,
-		repo string,
-		dryRun bool,
-		) string {
-	args := make(map[string]string)
-	
-	revel.Unbind(args, "owner", owner)
-	revel.Unbind(args, "repo", repo)
-	revel.Unbind(args, "dryRun", dryRun)
-	return revel.MainRouter.Reverse("Api.Unsubscribe", args).Url
 }
 
 
--- app/tmp/main.go
+++ app/tmp/main.go
@@ -28,6 +28,21 @@ func main() {
 	revel.Init(*runMode, *importPath, *srcPath)
 	revel.INFO.Println("Running revel server")
 	
+	revel.RegisterController((*controllers.Api)(nil),
+		[]*revel.MethodType{
+			&revel.MethodType{
+				Name: "Unsubscribe",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "owner", Type: reflect.TypeOf((*string)(nil)) },
+					&revel.MethodArg{Name: "repo", Type: reflect.TypeOf((*string)(nil)) },
+					&revel.MethodArg{Name: "dryRun", Type: reflect.TypeOf((*bool)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			
+		})
+	
 	revel.RegisterController((*controllers.Application)(nil),
 		[]*revel.MethodType{
 			&revel.MethodType{
@@ -51,7 +66,7 @@ func main() {
 				Args: []*revel.MethodArg{ 
 				},
 				RenderArgNames: map[int][]string{ 
-					54: []string{ 
+					52: []string{ 
 						"authUrl",
 					},
 				},
@@ -60,21 +75,6 @@ func main() {
 				Name: "Auth",
 				Args: []*revel.MethodArg{ 
 					&revel.MethodArg{Name: "code", Type: reflect.TypeOf((*string)(nil)) },
-				},
-				RenderArgNames: map[int][]string{ 
-				},
-			},
-			
-		})
-	
-	revel.RegisterController((*controllers.Api)(nil),
-		[]*revel.MethodType{
-			&revel.MethodType{
-				Name: "Unsubscribe",
-				Args: []*revel.MethodArg{ 
-					&revel.MethodArg{Name: "owner", Type: reflect.TypeOf((*string)(nil)) },
-					&revel.MethodArg{Name: "repo", Type: reflect.TypeOf((*string)(nil)) },
-					&revel.MethodArg{Name: "dryRun", Type: reflect.TypeOf((*bool)(nil)) },
 				},
 				RenderArgNames: map[int][]string{ 
 				},
